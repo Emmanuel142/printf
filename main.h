13055d1b3544f23f385acdbaf655145435c6132a
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2023-10-15 17:28:33 -0700

    Header file containing functoin prototypes

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFFERSIZE 1024
+
+int _puts(const char *string);
+void print_number(int n);
+int _printf(const char *format, ...);
+void flush_buffer(void);
+void _putchar(char c);
+void print_unsign(unsigned int n);
+#endif
